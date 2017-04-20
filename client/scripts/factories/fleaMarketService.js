2b15800037fc90aac4be3695a1f1fa84ee5d6697
Chris Keller <cjameskeller@gmail.com>
2017-04-20 17:12:46 -0500

    pretty much done

--- client/scripts/factories/fleaMarketService.js
+++ client/scripts/factories/fleaMarketService.js
@@ -1,14 +1,14 @@
 //factory
 myApp.factory('fleaMarketService', function() {
 
-//class MarketItem {currentPrice, name, quanity, avePrice(), iniital price -> stretch}
+//class MarketItem {currentPrice, name, quantity, avePrice(), iniital price -> stretch}
 //Create Parent Class
 class FleaMarketItem {
-    constructor ( name, quantity, initialPrice, currentPrice, averagePrice, image ){
+    constructor ( name, quantity, currentPrice, averagingArray, averagePrice, image ){
       this.name = name;
       this.quantity = quantity;
-      this.initialPrice = initialPrice;
       this.currentPrice = currentPrice;
+      this.averagingArray = averagingArray;
       this.averagePrice = averagePrice;
       this.image = image;
     };
@@ -16,39 +16,39 @@ class FleaMarketItem {
   //children (smallElec, fruits, collectables)
 // Create Electronic Child
 class Electronic extends FleaMarketItem {
-    constructor ( name, quantity, initialPrice, currentPrice, averagePrice, image ) {
-      super ( name, quantity, initialPrice, currentPrice, averagePrice, image );
+    constructor ( name, quantity, currentPrice, averagingArray, averagePrice, image ) {
+      super ( name, quantity, currentPrice, averagingArray, averagePrice, image );
     }
 }
 // Create Fruit Child
 class Fruit extends FleaMarketItem {
-    constructor ( name, quantity, initialPrice, currentPrice , averagePrice, image) {
-      super ( name, quantity, initialPrice, currentPrice, averagePrice, image );
+  constructor ( name, quantity, currentPrice, averagingArray, averagePrice, image ) {
+    super ( name, quantity, currentPrice, averagingArray, averagePrice, image );
     }
 }
 // Create Collectable Child
 class Collectable extends FleaMarketItem {
-    constructor ( name, quantity, initialPrice, currentPrice, averagePrice, image ) {
-      super ( name, quantity, initialPrice, currentPrice, averagePrice, image );
+  constructor ( name, quantity, currentPrice, averagingArray, averagePrice, image ) {
+    super ( name, quantity, currentPrice, averagingArray, averagePrice, image );
     }
 }
 // Electronic Children
-var toaster = new Electronic('Toaster', 0, 6.43, 2, 6.43, 'views/images/toaster.png');
-var lamp = new Electronic('Lamp', 0, 5.23, 2, 5.23, 'views/images/lamp.png');
-var bluRayPlayer = new Electronic('Blu Ray Player', 0, 8.15, 2, 8.15, 'views/images/blue_ray_player.png');
-var clock = new Electronic('Clock', 0, 4.44, 2, 4.44, 'views/images/clock.png');
+var toaster = new Electronic('Toaster', 0, 6.43,[], 6.43, 'views/images/toaster.png');
+var lamp = new Electronic('Lamp', 0, 5.23,[], 5.23, 'views/images/lamp.png');
+var bluRayPlayer = new Electronic('Blu Ray Player', 0, 8.15,[], 8.15, 'views/images/blue_ray_player.png');
+var clock = new Electronic('Clock', 0, 4.44,[], 4.44, 'views/images/clock.png');
 
 // Fruit Children
-var apple = new Fruit('Apple', 0, 0.45, 2, 0.45, 'views/images/apple.png');
-var orange = new Fruit('Orange', 0, 1.22, 2, 1.22, 'views/images/orange.png');
-var banana = new Fruit('Banana', 0, 8.88, 2, 8.88, 'views/images/banana.png');
-var grapes = new Fruit('Grapes', 0, 3.32, 2, 3.32, 'views/images/grapes.png');
+var apple = new Fruit('Apple', 0, 0.45,[], 0.45, 'views/images/apple.png');
+var orange = new Fruit('Orange', 0, 1.22,[], 1.22, 'views/images/orange.png');
+var banana = new Fruit('Banana', 0, 8.88,[], 8.88, 'views/images/banana.png');
+var grapes = new Fruit('Grapes', 0, 3.32,[], 3.32, 'views/images/grapes.png');
 
 // Collectable Children
-var comicBook = new Collectable('Comic Books', 0, 3.41, 2, 3.41, 'views/images/fancy_stuffed_animal.png');
-var fancyStuffedAnimal = new Collectable('Fancy Stuffed Animals', 0, 5.46, 2, 5.46, 'views/images/toaster.png');
-var jewelry = new Collectable('Jewelry', 0, 7.55, 2, 7.55, 'views/images/jewelry.png');
-var wine = new Collectable('Wine', 0, 6.56, 2, 6.56, 'views/images/wine.png');
+var comicBook = new Collectable('Comic Books', 0, 3.41,[], 3.41, 'views/images/comic_book.png');
+var fancyStuffedAnimal = new Collectable('Fancy Stuffed Animals', 0, 5.46,[], 5.46, 'views/images/fancy_stuffed_animal.png');
+var jewelry = new Collectable('Jewelry', 0, 7.55,[], 7.55, 'views/images/jewelry.png');
+var wine = new Collectable('Wine', 0, 6.56,[], 6.56, 'views/images/wine.png');
 
 //
 var electronicObject = {items: [ toaster, lamp, bluRayPlayer, clock]};
@@ -64,28 +64,36 @@ let accountBalance = {
 let buyFunc = (thisItem) => {
   if((accountBalance.amount - thisItem.currentPrice) > 0){
     accountBalance.amount = accountBalance.amount - thisItem.currentPrice;
-    thisItem.avgArray.push(thisItem.currentPrice);
+    accountBalance.amount = Math.round(accountBalance.amount * 100)/100;
+    thisItem.averagingArray.push(thisItem.currentPrice);
+    thisItem.quantity++;
+    averagingFunc(thisItem);
   }
   else{
     return false;
   }
 };
 
-let averagingFunc = (thisItem) => {
-  let sum = 0;
-  for( var i = 0; i < thisItem.avgArray; i++ ){
-      sum += parseInt( thisItem.avgArray[i], 10 );
+var averagingFunc = (thisItem) => {
+  var sum = 0;
+  for( var i = 0; i < thisItem.averagingArray.length; i++ ){
+      sum += parseInt( thisItem.averagingArray[i], 10 );
+      console.log("sum is: ", sum);
   }
-  var avg = sum/thisItem.avgArray.length;
+  var avg = sum/thisItem.averagingArray.length;
+  console.log("avg is: ", avg);
+  thisItem.averagePrice = Math.round(avg * 100)/100;
 };
 
 let sellItem = (thisItem) => {
-  if((accountBalance.amount + thisItem.currentPrice) < 10){
-    accountBalance.amount = accountBalance.amount + thisItem.currentPrice;
-  }
-  else{
-    return false;
-  }
+    if(thisItem.quantity - 1 >= 0){
+      accountBalance.amount = accountBalance.amount + thisItem.currentPrice;
+      accountBalance.amount = Math.round(accountBalance.amount * 100)/100;
+      thisItem.quantity--;
+    }
+    // else{
+    //
+    // }
 };
 
 //adjPrice
@@ -120,7 +128,7 @@ function adjPrice(thisItem, priceAdjustment) {
 }
 
 function changePrice(thisItem, priceAdjustment) {
-  if((thisItem.currentPrice + priceAdjustment) > 0){
+  if((thisItem.currentPrice + priceAdjustment) > 0 && (thisItem.currentPrice + priceAdjustment) < 10){
     thisItem.currentPrice = Math.round((thisItem.currentPrice + priceAdjustment) * 100)/100;
     return thisItem.currentPrice;
   } else{
@@ -129,12 +137,12 @@ function changePrice(thisItem, priceAdjustment) {
 }
 
   return {
-    // FleaMartetItem : FleaMarketItem,
     fruitObject : fruitObject,
     collectableObject : collectableObject,
     electronicObject : electronicObject,
     accountBalance : accountBalance,
     updater: updater,
-    sellItem: sellItem
+    sellItem: sellItem,
+    buyFunc: buyFunc
   };
 });
